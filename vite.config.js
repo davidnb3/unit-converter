1601b6c4e62f925eea862cc452eae0457d072ee3
david-mario.pieri <david-mario.pieri@ionos.com>
2026-03-31 16:58:17 +0200

    Update base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/keyfinder/',
+  base: '/unit-converter/',
   plugins: [
     react(),
     VitePWA({
@@ -17,7 +17,7 @@ export default defineConfig({
         background_color: '#E8E2DA',
         display: 'standalone',
         orientation: 'any',
-        start_url: '/',
+        start_url: '/unit-converter/',
         icons: [
           {
             src: '/icons/icon.svg',
